e94b43378c08d041a7c1c44e89295ff3b7a61bf5
Adam Pash <adam.pash@gmail.com>
2017-01-20 14:12:26 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Install
+
+```bash
+git clone git@github.com:adampash/serverless-babel-starter.git project_name
+cd project_name
+rm -rf .git
+git init && git add && git commit -m "Initial commit"
+yarn
+```
+
+## Run the hello function (with live reloading)
+
+```bash
+yarn watch:hello
+```
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/handler.js',
+  target: 'node',
+  externals: [
+    nodeExternals(),
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: __dirname,
+      exclude: /node_modules/,
+    }]
+  }
+};
