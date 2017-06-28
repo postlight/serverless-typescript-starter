a9ae11685ad4f63c5f5e7a5abd0adc6f02c57f9a
Adam Pash <adam.pash@gmail.com>
2017-06-28 09:20:47 -0700

    small refactor

--- src/hello.js
+++ src/hello.js
@@ -1,12 +1,12 @@
 import { successResponse } from './utils/lambda-response';
 
 const hello = (event, context, callback) => {
-  const response = {
+  const response = successResponse({
     message: 'Go Serverless v1.0! Your function executed successfully!',
     input: event,
-  };
+  });
 
-  callback(null, successResponse(response));
+  callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
