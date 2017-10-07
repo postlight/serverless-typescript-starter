08f858796b8d50613777c0a8ad1e2e4cc725201e
Adam Pash <adam.pash@gmail.com>
2017-10-07 14:17:22 -0700

    deps: update

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const nodeExternals = require('webpack-node-externals');
+const slsw = require('serverless-webpack')
 
 module.exports = {
-  entry: './src/handler.js',
+  entry: slsw.lib.entries,
   target: 'node',
   externals: [nodeExternals()],
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel'],
+      loaders: ['babel-loader'],
       include: __dirname,
       exclude: /node_modules/,
     }],
