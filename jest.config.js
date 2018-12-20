786129d740e60ef92a21abf22a7b3ceca4f94d8d
Adam Pash <adam.pash@gmail.com>
2018-12-20 12:55:59 -0800

    update jest to handle typescript

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+	"testRegex": "(.*\\.test\\.(tsx?|jsx?))$",
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+}
