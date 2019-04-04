5e47857fefdb7d93c8010c4ef5c9f8c4b4bb9d87
JadTermsani <jadtermsani@gmail.com>
2019-04-04 12:22:49 +0300

    fix: whitespace

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,9 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
-	"testRegex": "(.*\\.test\\.(tsx?|jsx?))$",
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": "babel-jest"
+  roots: ['<rootDir>/src'],
+  testRegex: '(.*\\.test\\.(tsx?|jsx?))$',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-}
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
