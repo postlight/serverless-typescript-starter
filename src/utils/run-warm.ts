ab7d7dc75a8b587b9f47417159ab6dfe0da45a6c
Wajeeh Zantout <wajeeh.zantout@gmail.com>
2019-12-17 14:19:28 +0200

    refactor: leave typescript function only

--- src/utils/run-warm.ts
+++ src/utils/run-warm.ts
@@ -1,7 +1,8 @@
-const runWarm = (lambdaFunc: Function): AWSLambda.Handler => async (
-  event,
-  context,
-) => {
+const runWarm = (lambdaFunc: Function): Function => async (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  event: any,
+  context: AWSLambda.Context
+): Promise<Response | string> => {
   // Detect the keep-alive ping from CloudWatch and exit early. This keeps our
   // lambda function running hot.
   if (event.source === 'serverless-plugin-warmup') {
